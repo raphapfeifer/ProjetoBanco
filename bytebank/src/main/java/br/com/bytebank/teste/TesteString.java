650faaaceb39da3155a2062651a9bd2124431f54
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2018-10-15 22:15:37 -0300

    15/10/2018

---
+++ bytebank/src/main/java/br/com/bytebank/teste/TesteString.java
@@ -0,0 +1,20 @@
+package br.com.bytebank.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		
+		String nome = "Alura";
+		
+		char c = nome.charAt(2);
+		System.out.println(c);
+		
+		//String outra = nome.replace("A", "a");
+		
+		//String outra = nome.toUpperCase();
+		
+		//System.out.println(outra);
+
+	}
+
+}
