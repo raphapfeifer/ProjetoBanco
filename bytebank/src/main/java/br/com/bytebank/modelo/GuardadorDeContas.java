cc27c641b559eb7f6f628c3f4eb677b5f2d993af
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2018-11-29 01:40:30 -0200

    29/11/2018

---
+++ bytebank/src/main/java/br/com/bytebank/modelo/GuardadorDeContas.java
@@ -0,0 +1,25 @@
+package br.com.bytebank.modelo;
+
+public class GuardadorDeContas {
+	
+	private Conta[] referencias;
+	private int posicaoLivre;
+	
+	public GuardadorDeContas(){
+		this.referencias = new Conta[10];
+	}
+	
+	public void adiciona(Conta ref){
+		this.referencias[this.posicaoLivre] = ref;
+		this.posicaoLivre ++;
+	}
+
+	public int getQuantidadeDeElementos() {
+		return this.posicaoLivre;
+	}
+
+	public Conta getReferencia(int pos) {
+		return this.referencias[pos];
+	}
+
+}
